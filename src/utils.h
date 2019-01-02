61e145bf66536a8ded67dcb383ae65cc4a194c76
minterciso <minterciso@gmail.com>
2019-01-01 22:26:05 -0200

    * Added the utils documentation

--- src/utils.h
+++ src/utils.h
@@ -7,8 +7,24 @@
 
 gsl_rng *prng;
 
+/**
+ * @brief start_prng Start the PRNG based on the Mersenne Twister, this can be overriden with the GSL_RNG_TYPE variable at runtime
+ * @return 0 if it was possible to start the PRNG, -1 otherwise
+ */
 int start_prng(void);
+
+/**
+ * @brief to_decimal Transform an unsigned int array of base 'base' in decimal
+ * @param str An unsigned int array
+ * @param base The base of the str array
+ * @param len The size of the str array
+ * @return The decimal number
+ */
 int to_decimal(unsigned int *str, int base, int len);
+
+/**
+ * @brief stop_prng Free the PRNG memory
+ */
 void stop_prng(void);
 
 #endif // UTILS_H
